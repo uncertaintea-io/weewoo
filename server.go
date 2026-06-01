656883c18bb255754443f4539e7154d3a6fbe0c1
Brian Rippy <rippyb123@gmail.com>
2026-06-01 15:33:45 -0400

    transferred over files from a previous project (#1)
    * transferred over files from a previous project
    
    * fixed the requests made by trippy, now the server runs

---
+++ server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"context"
+	"net/http"
+
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+)
+
+func main() {
+
+	//API routes
+
+	//Serve files from static folder
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+
+	//Serve api /TonyRippyis
+	http.HandleFunc("/TonyRippyis", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "old, weak, bald, and short")
+	})
+
+	port := ":5000"
+	fmt.Println("Server is running on port" + port)
+
+	//Start server on port specified above
+	log.Fatal(http.ListenAndServe(port, nil))
+
+	//Get Prometheus targets
+	config := api.Config{
+		Address: "http://localhost:9090",
+	}
+	client, err := api.NewClient(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	API := v1.NewAPI(client)
+	targets, err := API.Targets(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, at := range targets.Active {
+		fmt.Println(at.ScrapeURL)
+	}
+}
