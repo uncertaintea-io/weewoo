0dfe4f08521697266811aa836a8a195ada42a235
Brian Rippy <rippyb123@gmail.com>
2026-06-02 11:15:39 -0400

    adding server shut down (#3)
    * adding server shut down
    
    * responding to code feedback regarding server shutdown
    
    * adressing outdated comments

--- server.go
+++ server.go
@@ -1,47 +1,55 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
-	"context"
 	"net/http"
-
-	"github.com/prometheus/client_golang/api"
-	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 func main() {
 
-	//API routes
-
 	//Serve files from static folder
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 
-	//Serve api /TonyRippyis
-	http.HandleFunc("/TonyRippyis", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "old, weak, bald, and short")
+	//Serve api /Test
+	http.HandleFunc("/Test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello world!")
 	})
 
-	port := ":5000"
-	fmt.Println("Server is running on port" + port)
+	// monitorPort := ":5000"
+	appPort := ":8080"
+	fmt.Println("Server is running on port" + appPort)
 
-	//Start server on port specified above
-	log.Fatal(http.ListenAndServe(port, nil))
-
-	//Get Prometheus targets
-	config := api.Config{
-		Address: "http://localhost:9090",
+	appServer := &http.Server{
+		Addr:           appPort,
+		Handler:        nil,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
 	}
-	client, err := api.NewClient(config)
-	if err != nil {
+
+	serverErr := make(chan error)
+	// go func() {
+	// 	serverErr <- http.ListenAndServe(monitorPort, nil)
+	// }()
+
+	go func() {
+		serverErr <- appServer.ListenAndServe()
+	}()
+
+	signalCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	select {
+	case <-signalCtx.Done():
+
+	case err := <-serverErr:
 		log.Fatal(err)
 	}
-	API := v1.NewAPI(client)
-	targets, err := API.Targets(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, at := range targets.Active {
-		fmt.Println(at.ScrapeURL)
-	}
+
+	appServer.Shutdown(context.Background())
 }
