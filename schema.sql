e2f18fb7556c0216722eff8edb004839bf40130a
Brian Rippy <rippyb123@gmail.com>
2026-06-16 14:22:44 -0400

    added the schema file with table information (#39)
    * added the schema file with table information
    
    * fixed issue that codex had

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE data_source ( id int PRIMARY KEY, type varchar, url varchar, polling_interval int);
+CREATE TABLE config (key varchar, value varchar);
+CREATE TABLE alert_sink (id int PRIMARY KEY, type varchar, url varchar);
+CREATE TABLE time_chunk (service_id int, indicator_id int, sample_timestamp int, chunk bytea);
+CREATE TABLE service (id int PRIMARY KEY, name varchar);
+CREATE TABLE verdict (sample_timestamp int, good boolean, pvalue float);
