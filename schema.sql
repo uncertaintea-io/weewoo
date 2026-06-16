0e2ea5f5442191f7b4c583fe7bbf148bd60348af
Brian Rippy <rippyb123@gmail.com>
2026-06-16 14:21:55 -0400

    fixed issue that codex had

--- schema.sql
+++ schema.sql
@@ -2,5 +2,5 @@ CREATE TABLE data_source ( id int PRIMARY KEY, type varchar, url varchar, pollin
 CREATE TABLE config (key varchar, value varchar);
 CREATE TABLE alert_sink (id int PRIMARY KEY, type varchar, url varchar);
 CREATE TABLE time_chunk (service_id int, indicator_id int, sample_timestamp int, chunk bytea);
-CREATE TABLE service (id int, name varchar);
+CREATE TABLE service (id int PRIMARY KEY, name varchar);
 CREATE TABLE verdict (sample_timestamp int, good boolean, pvalue float);
