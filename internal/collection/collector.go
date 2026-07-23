d0f72f2dd1c84e61fb8407a1fa8121e7e672b5ec
Brian Rippy <rippyb123@gmail.com>
2026-07-23 08:12:38 -0400

    tiny fixes

--- internal/collection/collector.go
+++ internal/collection/collector.go
@@ -52,8 +52,10 @@ func (c *collector) Schedule(service *config.Service) error {
 	return c.scheduler.AddCallback(service.Id, service.Interval, func(ctx context.Context, start time.Time, end time.Time) IntervalResult {
 		slog.Info("Collecting sample", "service", service.Name, "start", start, "end", end)
 		if err := c.collectSamples(ctx, service, start, end); err != nil {
+			slog.Error("Failed to collect samples", "error", err)
 			return IntervalRetry(err)
 		}
+		slog.Info("Collected samples", "service", service.Name, "start", start, "end", end)
 		return IntervalSuccess()
 	})
 }
