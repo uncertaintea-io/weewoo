dd4ffb081f02bb9ffa7076f1497ac146b6bafd55
Tony Rippy <trippy@uncertaintea.io>
2026-08-04 13:19:12 -0400

    Allow jecdf boundary error of 3 ULPs (#131)

--- internal/ecdf/build.go
+++ internal/ecdf/build.go
@@ -63,7 +63,7 @@ func buildError(prefix string, err error) error {
 func buildFromStream(ctx context.Context, chunks <-chan []byte, writer io.Writer) error {
 	return runJECDF(
 		ctx,
-		[]string{"build"},
+		[]string{"build", "-ulp", "3"},
 		func(ctx context.Context, stdin io.Writer) error {
 			if err := writeChunks(ctx, stdin, chunks); err != nil {
 				return fmt.Errorf("failed to write chunks to jecdf: %w", err)
